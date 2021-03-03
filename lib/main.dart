71db95f0834b4ea6a6d9cb43584a6c658edcd7fc
Katie <25576005+stuttskl@users.noreply.github.com>
2021-03-03 09:49:53 -0800

    implemented photo picker feature

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
+import 'dart:io';
+
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_storage/firebase_storage.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-
-import './widgets/data_screen.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -36,29 +37,64 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  File image;
+  final picker = ImagePicker();
+  Future getImage() async {
+    print("inside of get image");
+    final pickedFile = await picker.getImage(source: ImageSource.gallery);
+    image = File(pickedFile.path);
+
+    setState(() {
+      if (pickedFile != null) {
+        image = File(pickedFile.path);
+      } else {
+        print('No image selected');
+      }
+    });
+    // Reference storageReference = FirebaseStorage.instance
+    //     .ref()
+    //     .child('example.jpg'); // Path.basename(image.path) as alt
+    // UploadTask uploadTask = storageReference.putFile(image);
+    // await uploadTask.onComplete;
+    // BREAKING: isCanceled, isComplete, isInProgress, isPaused and isSuccessful have now been removed.
+    // Instead, you should subscribe to the stream (for paused/progress/complete/error events) or the task Future for task completion/errors.
+    // final url = await storageReference.getDownloadURL();
+    // print(url);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text(widget.title),
-        ),
-        body: StreamBuilder(
-            stream:
-                FirebaseFirestore.instance.collection('example').snapshots(),
-            builder: (context, record) {
-              if (record.hasData) {
-                return ListView.builder(
-                  itemCount: record.data.docs.length,
-                  itemBuilder: (context, index) {
-                    var post = record.data.docs[index];
-                    return ListTile(
-                        leading: Text(post['name']),
-                        title: Text(post['weight'].toString()));
-                  },
-                );
-              } else {
-                return Center(child: CircularProgressIndicator());
-              }
-            }));
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+          child: image == null ? Text('no image selected') : Image.file(image)),
+      floatingActionButton: FloatingActionButton(
+        onPressed: getImage,
+        tooltip: 'Pick Image',
+        child: Icon(Icons.add_a_photo),
+      ),
+      // body: StreamBuilder(
+      //     stream:
+      //         FirebaseFirestore.instance.collection('example').snapshots(),
+      //     builder: (context, record) {
+      //       if (record.hasData &&
+      //           record.data.docs != null &&
+      //           record.data.docs.length > 0) {
+      //         return ListView.builder(
+      //           itemCount: record.data.docs.length,
+      //           itemBuilder: (context, index) {
+      //             var post = record.data.docs[index];
+      //             return ListTile(
+      //                 leading: Text(post['name']),
+      //                 title: Text(post['weight'].toString()));
+      //           },
+      //         );
+      //       } else {
+      //         return Center(child: CircularProgressIndicator());
+      //       }
+      //     })
+    );
   }
 }
