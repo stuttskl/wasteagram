7c8036afbc3575fe413b3f02d48a9f9340d74f72
Katie <25576005+stuttskl@users.noreply.github.com>
2021-03-03 10:42:38 -0800

    created Post model

---
+++ lib/models/post.dart
@@ -0,0 +1,9 @@
+class Post {
+  final String imgUrl;
+  final int lat;
+  final int long;
+  final int numWasted;
+  final String timeStamp;
+
+  Post({this.imgUrl, this.lat, this.long, this.numWasted, this.timeStamp});
+}
--- lib/screens/list_screen.dart
+++ lib/screens/list_screen.dart
@@ -6,15 +6,22 @@ class ListScreen extends StatefulWidget {
 }
 
 class _ListScreenState extends State<ListScreen> {
+  void goToDetailsScreen(context, id) {
+    print("inside of go to details screen for id: " + id.toString());
+    // Navigator.pushNamed(context, 'detailsScreen', arguments:)
+  }
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
       itemCount: 5,
       itemBuilder: (context, index) {
         return ListTile(
-          leading: Text('Hello Title'),
-          title: Text('Hello subtitle'));
+          title: Text('Wednesday, March 3rd 2021'), // date of post
+          trailing: Text(1.toString()), // number of items wasted
+          onTap:  () => goToDetailsScreen(context, index) // might have to -1 from this idk yet
+        );
       },
     );
+    // add photo FAB here
   }
 }
