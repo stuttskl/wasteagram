276feab87ed7d3920d70c6f7ced56ad891ccc6b4
Katie <25576005+stuttskl@users.noreply.github.com>
2021-03-04 08:44:14 -0800

    fixed date format for displaying posts

--- lib/screens/list_screen.dart
+++ lib/screens/list_screen.dart
@@ -1,5 +1,5 @@
 import 'dart:io';
-
+import 'package:intl/intl.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
@@ -19,7 +19,7 @@ class _ListScreenState extends State<ListScreen> {
   final picker = ImagePicker();
 
   Future getImage() async {
-    print("inside of get image");
+    print("inside of get image top");
     final pickedFile = await picker.getImage(source: ImageSource.gallery);
     image = File(pickedFile.path);
 
@@ -30,10 +30,11 @@ class _ListScreenState extends State<ListScreen> {
         print('No image selected');
       }
     });
+    print("after set state");
 
     Reference storageReference = FirebaseStorage.instance
         .ref()
-        .child('example.jpg'); // Path.basename(image.path) as alt
+        .child(DateTime.now().toIso8601String()); // saving the image file name as a date time string to ensure uniqueness
 
     UploadTask uploadTask = storageReference.putFile(image);
 
@@ -43,9 +44,12 @@ class _ListScreenState extends State<ListScreen> {
 
     FirebaseFirestore.instance.collection('posts').add({
       'imgUrl': url,
-      'numWasted': 22
+      'numWasted': 22,
+      'timeStamp': DateTime.now().toIso8601String()
     });
   }
+
+  // this needs work. id is off by 1
   void goToDetailsScreen(context, id) {
     print("inside of go to details screen for id: " + id.toString());
     Navigator.pushNamed(context, 'detailsScreen',
@@ -54,7 +58,7 @@ class _ListScreenState extends State<ListScreen> {
         lat: 0,
         long: 0,
         numWasted: 0,
-        timeStamp: "test"
+        timeStamp: DateTime.now().toIso8601String()
       )
     );
   }
@@ -64,26 +68,27 @@ class _ListScreenState extends State<ListScreen> {
     return Scaffold(
       appBar: AppBar(title: Text('wasteagram - # of wasted items')),
       body: StreamBuilder(
-          stream: FirebaseFirestore.instance.collection('posts').snapshots(),
-          builder: (context, record) {
-            if (record.hasData &&
-                record.data.docs != null &&
-                record.data.docs.length > 0) {
-              return ListView.builder(
-                itemCount: record.data.docs.length,
-                itemBuilder: (context, index) {
-                  var post = record.data.docs[index];
-                  return ListTile(
-                    leading: Text(post['imgUrl']),
-                    title: Text(post['numWasted'].toString()),
-                    onTap: () => goToDetailsScreen(context, index - 1) // might be an issuew tih this
-                  );
-                },
-              );
-            } else {
-              return Center(child: CircularProgressIndicator());
-            }
-          }),
+        stream: FirebaseFirestore.instance.collection('posts').snapshots(),
+        builder: (context, record) {
+          if (record.hasData &&
+              record.data.docs != null &&
+              record.data.docs.length > 0) {
+            return ListView.builder(
+              itemCount: record.data.docs.length,
+              itemBuilder: (context, index) {
+                var post = record.data.docs[index];
+                return ListTile(
+                  leading: Text(DateFormat('yMMMMEEEEd').format(DateTime.parse(post['timeStamp'].toString()))),
+                  trailing: Text(post['numWasted'].toString()),
+                  onTap: () => goToDetailsScreen(context, index - 1) // might be an issuew tih this
+                );
+              },
+            );
+          } else {
+            return Center(child: CircularProgressIndicator());
+          }
+        }
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: () => getImage(),
         tooltip: 'Add New Image',
